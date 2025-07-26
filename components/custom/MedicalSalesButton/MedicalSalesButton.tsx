b785e2c468b4fff76024347a58710852b2dc0177
krishna-vamsi-14 <durgakrishnavamsi@gmail.com>
2025-07-26 17:42:40 +0530

    minor ui button bg color fix | Krishna

--- components/custom/MedicalSalesButton/MedicalSalesButton.tsx
+++ components/custom/MedicalSalesButton/MedicalSalesButton.tsx
@@ -18,7 +18,7 @@ export const MedicalSalesButton = ({
   return (
     <Button
       onClick={onClick}
-      className={`bg-[${backgroundColor}] text-[${color}] px-[20px] py-[8px] rounded-[6px] flex flex-row gap-[6px] items-center hover:bg-[#F7F7F9] hover:text-[${color}]`}
+      className={`bg-white text-[${color}] px-[20px] py-[8px] rounded-[6px] flex flex-row gap-[6px] items-center hover:bg-[#F7F7F9] hover:text-[${color}]`}
     >
       {icon}
       {label}
